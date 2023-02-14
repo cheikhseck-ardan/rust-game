5a24f8e3f50a1904b4ce6c840998738c66dd92a8
cheikhseck-ardan <cheikh.seck@ardanlabs.com>
2023-02-14 04:09:40 +0000

    make box stay

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,13 @@ impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         let col2 = RGB::named(YELLOW);
 
-        ctx.cls();
+        ctx.set_scale(
+        	1.05,
+        	1,
+        	1
+        );
+
+        ctx.cls_bg(RGB::named(WHITE));
         ctx.draw_bar_horizontal(
 		    0,
 		    40,
@@ -21,28 +27,32 @@ impl GameState for State {
 		    col2
 		);
 
+        ctx.draw_box_double(
+		    10,
+		    self.y,
+		    5,
+		    5,
+		    RGB::named(RED),
+		    RGB::named(RED)
+		);
+
         if self.going_down {
             self.y += 1;
-            if self.y > 48 {
+            if self.y > 34 {
                 self.going_down = false;
             }
-        } else {
-            self.y -= 1;
-            if self.y < 2 {
-                self.going_down = true;
-            }
-        }
+        } 
 
-        ctx.draw_box(51, 0, 20, 3, RGB::named(WHITE), RGB::named(BLACK));
+        ctx.draw_box(36, 0, 20, 3, RGB::named(WHITE), RGB::named(BLACK));
         ctx.printer(
-            70,
+            55,
             1,
             &format!("#[pink]FPS: #[]{}", ctx.fps),
             TextAlign::Right,
             None,
         );
         ctx.printer(
-            70,
+            55,
             5,
             &format!("#[pink]Frame Time: #[]{} ms", ctx.frame_time_ms),
             TextAlign::Right,
@@ -65,6 +75,7 @@ fn main() -> BError {
         going_down: true
     };
 
-  
+    register_palette_color("pink", RGB::named(MAGENTA));
+
     main_loop(context, gs)
 }
