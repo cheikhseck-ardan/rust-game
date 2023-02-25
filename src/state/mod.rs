ad6773eb1e486c8d193d083e089e0046893fe973
William Kennedy <bill@ardanlabs.com>
2023-02-25 16:04:12 -0500

    using rust fmt for code formatting

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -1,11 +1,11 @@
 // State contains all the game state and logic.
 
 pub const TOP_SCREEN_PIXEL: i32 = 8;
-pub const BOX_HEIGHTWIDTH:  i32 = 5;
-pub const GROUND_PIXEL:     i32 = 45;
-pub const GROUND_WIDTH:     i32 = 80;
-pub const GAME_WINDOW:      i32 = 50;
-pub const GROUND_COLLISION: i32 = GROUND_PIXEL-BOX_HEIGHTWIDTH;
+pub const BOX_HEIGHTWIDTH: i32 = 5;
+pub const GROUND_PIXEL: i32 = 45;
+pub const GROUND_WIDTH: i32 = 80;
+pub const GAME_WINDOW: i32 = 50;
+pub const GROUND_COLLISION: i32 = GROUND_PIXEL - BOX_HEIGHTWIDTH;
 
 enum Moving {
     Not,
@@ -16,15 +16,15 @@ enum Moving {
 // =============================================================================
 
 pub struct State {
-    box_y:       i32,    // Box's veritical position.
-    box_moving:  Moving, // Direction the box is moving.
+    box_y: i32,         // Box's veritical position.
+    box_moving: Moving, // Direction the box is moving.
 }
 
 pub fn new() -> State {
     return State {
-        box_y:      GROUND_COLLISION,
+        box_y: GROUND_COLLISION,
         box_moving: Moving::Not,
-    }
+    };
 }
 
 impl rltk::GameState for State {
@@ -37,21 +37,21 @@ impl rltk::GameState for State {
 impl State {
     fn keyboard_input(&mut self, ctx: &mut rltk::Rltk) {
         match ctx.key {
-            None => {},
+            None => {}
             Some(key) => match key {
                 rltk::VirtualKeyCode::Space => {
                     if self.box_y == GROUND_COLLISION {
                         self.box_moving = Moving::Up;
                     }
-                },
-                _ => {},
+                }
+                _ => {}
             },
         };
     }
 
     fn render(&mut self, ctx: &mut rltk::BTerm) {
         ctx.cls_bg(rltk::RGB::named(rltk::WHITE));
-       
+
         ctx.draw_bar_horizontal(
             0,                              // x
             TOP_SCREEN_PIXEL,               // y
@@ -87,18 +87,25 @@ impl State {
                 if self.box_y == GROUND_COLLISION {
                     self.box_moving = Moving::Not;
                 }
-            },
+            }
             Moving::Up => {
                 self.box_y -= 1;
                 if self.box_y == TOP_SCREEN_PIXEL {
                     self.box_moving = Moving::Down;
                 }
-            },
-            _ => {},
+            }
+            _ => {}
         }
 
-        ctx.draw_box(36, 0, 20, 3, rltk::RGB::named(rltk::WHITE), rltk::RGB::named(rltk::BLACK));
-        
+        ctx.draw_box(
+            36,
+            0,
+            20,
+            3,
+            rltk::RGB::named(rltk::WHITE),
+            rltk::RGB::named(rltk::BLACK),
+        );
+
         ctx.printer(
             55,
             1,
@@ -106,7 +113,7 @@ impl State {
             rltk::TextAlign::Right,
             None,
         );
-        
+
         ctx.printer(
             55,
             5,
