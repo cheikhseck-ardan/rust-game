a0e844ea847a146c3252f04ae511a2661b9dd4b5
William Kennedy <bill@ardanlabs.com>
2023-02-25 15:12:52 -0500

    adding enum

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 // main initialized the game and starts the game loop.
 
-mod state;
+pub mod state;
 
 fn main() -> rltk::BError {
     let context = rltk::BTermBuilder::simple80x50()
         .with_title("Hello Bracket World")
         .build()?;
 
-    rltk::main_loop(context, state::State::new())
+    rltk::main_loop(context, state::new())
 }
--- src/state.rs
+++ src/state.rs
@@ -7,15 +7,24 @@ pub const GROUND_WIDTH:     i32 = 80;
 pub const GAME_WINDOW:      i32 = 50;
 pub const GROUND_COLLISION: i32 = GROUND_PIXEL-BOX_HEIGHTWIDTH;
 
-pub const MOVING_NONE: i32 = 0;
-pub const MOVING_UP:   i32 = 1;
-pub const MOVING_DOWN: i32 = 2;
+enum Moving {
+    Not,
+    Up,
+    Down,
+}
 
 // =============================================================================
 
 pub struct State {
-    box_y:       i32,  // Box's veritical position.
-    box_moving:  i32,  // Direction the box is moving.
+    box_y:       i32,    // Box's veritical position.
+    box_moving:  Moving, // Direction the box is moving.
+}
+
+pub fn new() -> State {
+    return State {
+        box_y:      GROUND_COLLISION,
+        box_moving: Moving::Not,
+    }
 }
 
 impl rltk::GameState for State {
@@ -26,87 +35,84 @@ impl rltk::GameState for State {
 }
 
 impl State {
-    pub fn new() -> Self {
-        return Self {
-            box_y:      GROUND_COLLISION,
-            box_moving: MOVING_NONE,
-        }
+    fn keyboard_input(&mut self, ctx: &mut rltk::Rltk) {
+        match ctx.key {
+            None => {},
+            Some(key) => match key {
+                rltk::VirtualKeyCode::Space => {
+                    if self.box_y == GROUND_COLLISION {
+                        self.box_moving = Moving::Up;
+                    }
+                },
+                _ => {},
+            },
+        };
     }
 
-   fn keyboard_input(&mut self, ctx: &mut rltk::Rltk) {
-       match ctx.key {
-           None => {}
-           Some(key) => match key {
-                rltk::VirtualKeyCode::Space => {
-                   if self.box_y == GROUND_COLLISION {
-                       self.box_moving = MOVING_UP;
-                   }
-               },
-               _ => {},
-           },
-       };
-   }
-
-   fn render(&mut self, ctx: &mut rltk::BTerm) {
-       ctx.cls_bg(rltk::RGB::named(rltk::WHITE));
+    fn render(&mut self, ctx: &mut rltk::BTerm) {
+        ctx.cls_bg(rltk::RGB::named(rltk::WHITE));
        
-       ctx.draw_bar_horizontal(
-           0,                  // x
-           TOP_SCREEN_PIXEL,   // y
-           GROUND_WIDTH,       // width
-           GAME_WINDOW,        // n
-           GAME_WINDOW,        // max
-           rltk::RGB::named(rltk::YELLOW), // foreground color
-           rltk::RGB::named(rltk::YELLOW), // background color
-       );
+        ctx.draw_bar_horizontal(
+            0,                              // x
+            TOP_SCREEN_PIXEL,               // y
+            GROUND_WIDTH,                   // width
+            GAME_WINDOW,                    // n
+            GAME_WINDOW,                    // max
+            rltk::RGB::named(rltk::YELLOW), // foreground color
+            rltk::RGB::named(rltk::YELLOW), // background color
+        );
 
-       ctx.draw_bar_horizontal(
-           0,                  // x
-           GROUND_PIXEL,       // y
-           GROUND_WIDTH,       // width
-           GAME_WINDOW,        // n
-           GAME_WINDOW,        // max
-           rltk::RGB::named(rltk::YELLOW), // foreground color
-           rltk::RGB::named(rltk::YELLOW), // background color
-       );
+        ctx.draw_bar_horizontal(
+            0,                              // x
+            GROUND_PIXEL,                   // y
+            GROUND_WIDTH,                   // width
+            GAME_WINDOW,                    // n
+            GAME_WINDOW,                    // max
+            rltk::RGB::named(rltk::YELLOW), // foreground color
+            rltk::RGB::named(rltk::YELLOW), // background color
+        );
 
-       ctx.draw_box_double(
-           10,              // x
-           self.box_y,          // y
-           BOX_HEIGHTWIDTH, // width
-           BOX_HEIGHTWIDTH, // height
-           rltk::RGB::named(rltk::RED), // foreground color
-           rltk::RGB::named(rltk::RED), // background color
-       );
+        ctx.draw_box_double(
+            10,                          // x
+            self.box_y,                  // y
+            BOX_HEIGHTWIDTH,             // width
+            BOX_HEIGHTWIDTH,             // height
+            rltk::RGB::named(rltk::RED), // foreground color
+            rltk::RGB::named(rltk::RED), // background color
+        );
 
-       if self.box_moving == MOVING_DOWN {
-           self.box_y += 1;
-           if self.box_y == GROUND_COLLISION {
-               self.box_moving = MOVING_NONE;
-           }
-       }
+        match self.box_moving {
+            Moving::Down => {
+                self.box_y += 1;
+                if self.box_y == GROUND_COLLISION {
+                    self.box_moving = Moving::Not;
+                }
+            },
+            Moving::Up => {
+                self.box_y -= 1;
+                if self.box_y == TOP_SCREEN_PIXEL {
+                    self.box_moving = Moving::Down;
+                }
+            },
+            _ => {},
+        }
 
-       if self.box_moving == MOVING_UP {
-           self.box_y -= 1;
-           if self.box_y == TOP_SCREEN_PIXEL {
-               self.box_moving = MOVING_DOWN;
-           }
-       }
-
-       ctx.draw_box(36, 0, 20, 3, rltk::RGB::named(rltk::WHITE), rltk::RGB::named(rltk::BLACK));
-       ctx.printer(
-           55,
-           1,
-           &format!("#[pink]FPS: #[]{}", ctx.fps),
-           rltk::TextAlign::Right,
-           None,
-       );
-       ctx.printer(
-           55,
-           5,
-           &format!("#[pink]Frame Time: #[]{} ms", ctx.frame_time_ms),
-           rltk::TextAlign::Right,
-           None,
-       );
-   }
+        ctx.draw_box(36, 0, 20, 3, rltk::RGB::named(rltk::WHITE), rltk::RGB::named(rltk::BLACK));
+        
+        ctx.printer(
+            55,
+            1,
+            &format!("#[pink]FPS: #[]{}", ctx.fps),
+            rltk::TextAlign::Right,
+            None,
+        );
+        
+        ctx.printer(
+            55,
+            5,
+            &format!("#[pink]Frame Time: #[]{} ms", ctx.frame_time_ms),
+            rltk::TextAlign::Right,
+            None,
+        );
+    }
 }
